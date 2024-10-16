2ef2d5d24b31930c9e68671bdac583797457f67d
Ebrahim Beiaty <ebrahimbeiaty@gmail.com>
2024-10-16 19:22:18 +0100

    upload image for chat bot

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+
 const messageInput = document.querySelector(".message-input");
 const chatBody = document.querySelector(".chat-body");
 const sendMessageButton = document.getElementById("send-message");
+const fileInput = document.getElementById("file-input");
+
+const API_KEY = "AIzaSyDgkpzr6v6zxF1sF_ILa24z_kxBFNfVu3o";
+
+const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY} `;
 
 const userData = {
-  message: null,
+    message: null,
+    file: {
+        data: null,
+        mime_type: null
+    }
 };
 
 // Handle keydown event for Enter key
@@ -13,6 +23,52 @@ messageInput.addEventListener("keydown", (e) => {
     handleOutgoingMessage(e, userMessage);
   }
 });
+//generate bot response
+const generateBotResponse = async (incomingMessageDiv) => {
+    const messageElement = incomingMessageDiv.querySelector('.message-text')
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      contents: [
+        {
+          parts: [{ text: userData.message }, ...(userData.file.data ? [{inline_data: userData.file }]: [])],
+        },
+      ],
+    }),
+  };
+  try {
+    const response = await fetch(API_URL, requestOptions);
+    const data = await response.json();
+    if (!response.ok) throw new Error(data.error.message);
+      const botMessage = data.candidates[0].content.parts[0].text.replace(/\*\*(.*?)\*\*/g, "$1").trim();
+      messageElement.textContent = botMessage;
+  } catch (error) {
+      console.log(error);
+      messageElement.textContent = "Error: " + error.message;
+      messageElement.style.color = "red";
+  } finally {
+      incomingMessageDiv.classList.remove("thinking");
+    chatBody.scrollTo({ top: chatBody.scrollHeight, behavior: "smooth" });
+
+      messageInput.focus();
+
+  }
+};
+  setTimeout(() => {
+    const messageContent = `  <div class="message bot-message">
+                <img src="./images/chatbot.png" width="50px" height="50px" alt="">
+                <div class="message-text">
+                    <p class="message">How can I help you?</p>
+                </div>
+            </div>`;
+    const incomingMessageDiv = createMessageElement(
+      messageContent,
+      "bot-message"
+    );
+      chatBody.appendChild(incomingMessageDiv);
+  }, 1000);
+
 
 // Create message element with dynamic classes
 const createMessageElement = (content, ...classes) => {
@@ -33,7 +89,10 @@ const handleOutgoingMessage = (e, userMessage) => {
   messageInput.value = "";
 
   // Create the message content with userData.message
-    const messageContent = `<div class="message-text"></div>`;
+    const messageContent = `<div class="message-text"></div>
+    ${userData.file.data ? `<img src="data:${userData.file.mime_type}; base64, ${userData.file.data}" class="attachment"/>` : ""}
+    `
+        ;
   const outgoingMessageDiv = createMessageElement(
     messageContent,
     "user-message"
@@ -43,6 +102,7 @@ const handleOutgoingMessage = (e, userMessage) => {
 
   // Append the message to the chat body
     chatBody.appendChild(outgoingMessageDiv);
+    chatBody.scrollTo({ top: chatBody.scrollHeight, behavior: "smooth" });
     setTimeout(() => {
 
     const messageContent = `  <div class="message bot-message thinking">
@@ -60,7 +120,13 @@ const handleOutgoingMessage = (e, userMessage) => {
       "bot-message",
       "think"
     );
-    chatBody.appendChild(incomingMessageDiv);
+        chatBody.appendChild(incomingMessageDiv);
+            chatBody.scrollTo({
+              top: chatBody.scrollHeight,
+              behavior: "smooth",
+            });
+
+        generateBotResponse(incomingMessageDiv);
      
     
 }, 600)
@@ -76,3 +142,25 @@ sendMessageButton.addEventListener("click", (e) => {
     handleOutgoingMessage(e, userMessage);
   }
 });
+
+
+
+// Handle file upload
+fileInput.addEventListener("change", (e) => {
+    const file = fileInput.files[0];
+    if (file) return;
+
+    const reader = new FileReader();
+    reader.onload = (e) => {
+        const base64String = e.target.split(',')[1];
+        userData.file= {
+            data: base64String,
+                mime_type: file.type
+        }
+        fileInput.value = '';
+    }
+    reader.readAsDataURL(file);
+    })
+
+
+document.getElementById('file-upload').addEventListener("click", ()=> fileInput.click())
